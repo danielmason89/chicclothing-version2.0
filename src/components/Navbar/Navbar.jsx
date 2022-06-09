29674c70825497243087ac1b65c475e91557488b
Daniel Mason <danmasson0@gmail.com>
2022-06-09 00:36:22 -0400

    Set-up full-stack e-commerce app with create-react-app, and started coding out navbar with respective styles and material-ui icons

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Search, ShoppingCartOutlined } from '@mui/icons-material';
+import { Badge } from '@mui/material';
+
+const Navbar = () => {
+    return (
+        <StyledNavbar>
+            <Wrapper>
+                <Left>
+                    <Language>
+                        EN
+                    </Language>
+                    <SearchContainer>
+                        <Input />
+                        <Search style={{ color: "gray", fontSize: "16" }} />
+                    </SearchContainer>
+                </Left>
+                <Center>
+                    <Logo>CHIC-C.</Logo>
+                </Center>
+                <Right>
+                    <MenuItem>Register</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={444} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge >
+                    </MenuItem>
+                </Right>
+            </Wrapper>
+        </StyledNavbar>
+    )
+}
+
+const StyledNavbar = styled.nav`
+height: 60px;
+
+`;
+
+const Wrapper = styled.div`
+paddinG: 10px 20px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`;
+
+const Left = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+`;
+
+const Language = styled.span`
+font-size: 14px;
+cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+border: 0.5px solid lightgrey;
+display: flex;
+align-items: center;
+margin-left: 25px;
+padding: 5px;
+`;
+
+const Input = styled.input`
+border: none;
+`;
+
+const Logo = styled.h1`
+font-weight: bold;
+`;
+const MenuItem = styled.div`
+font-size: 14px;
+cursor: pointer;
+margin-left: 25px;`
+
+const Right = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+justify-content: flex-end;`;
+const Center = styled.div`
+flex: 1;
+text-align: center;`;
+
+export default Navbar;
