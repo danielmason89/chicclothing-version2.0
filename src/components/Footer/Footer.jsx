3e891ac6e5f2c9b47acc43f01b7afee7fa02f50c
Daniel Mason <danmasson0@gmail.com>
2022-06-28 22:24:22 -0400

    finish implementing design for footer on homepage

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,44 @@
+import { Instagram, Twitter, Pinterest, Facebook, Map, Phone, MailOutline } from '@mui/icons-material'
 import styled from 'styled-components'
 
 const Footer = () => {
     return (
-        <Container>Footer</Container>
+        <Container>
+            <Left>
+                <Logo>Chic - Clothing</Logo>
+                <Description> There are many variations of passages of Lorem Ipsum available, but
+                    the majority have suffered alteration in some form, by injected
+                    humour, or randomized words which don’t look even slightly believable.
+                </Description>
+                <SocialContainer>
+                    <SocialIcon color="3B5999"><Facebook /></SocialIcon>
+                    <SocialIcon color="E4405F"><Instagram /></SocialIcon>
+                    <SocialIcon color="55ACEE"><Twitter /></SocialIcon>
+                    <SocialIcon color="E60023"><Pinterest /></SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem><Map style={{ marginRight: "10px" }} /> 622 Chic Path , South Chic 98336</ContactItem>
+                <ContactItem><Phone style={{ marginRight: "10px" }} /> +1 613 234 5678</ContactItem>
+                <ContactItem><MailOutline style={{ marginRight: "10px" }} /> danmasson0@gmail.con</ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
+        </Container>
     )
 }
 
@@ -12,16 +48,69 @@ display: flex;
 
 const Left = styled.div`
 flex: 1;
+display: flex;
+flex-direction: column;
+padding: 20px;
+`
+
+const Logo = styled.h1``
+
+const Description = styled.p`
+margin: 20px 0px;
+`
+
+const SocialContainer = styled.div`
+display: flex;
+`
+
+const SocialIcon = styled.div`
+width: 40px;
+height: 40px;
+border-radius: 50%;
+color: white;
+background-color: #${props => props.color};
+display: flex;
+align-items: center;
+justify-content: center;
+margin-bottom: 20px;
 `
 
 const Center = styled.div`
 flex: 1;
+padding :20px;
+`
+
+const Title = styled.h3`
+margin-bottom: 30px;`
+
+const List = styled.ul`
+margin:0;
+padding: 0;
+list-style: none;
+display: flex;
+flex-wrap: wrap;
+`
+const ListItem = styled.li`
+width: 50%;
+margin-bottom: 10pxs;
+
 `
 
 const Right = styled.div`
 flex: 1;
+padding :20px;
 `
 
+const ContactItem = styled.div`
+margin-bottom: 20px;
+display: flex;
+align-items: center;
+`
+
+
+const Payment = styled.img`
+width: 45%;
+`
 
 
 
