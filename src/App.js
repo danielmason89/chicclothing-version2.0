2ede5cff90c6cd3433c500a24af4005aeb3eed79
Daniel Mason <danmasson0@gmail.com>
2022-06-30 05:59:16 -0400

    implement login and register page designs for ecommerec app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./pages/Home/Home";
 import ProductList from "./pages/ProductList/ProductList";
 import ProductPage from "./pages/Product/Product";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <ProductPage />
+      <Login />
     </div>
   );
 }
